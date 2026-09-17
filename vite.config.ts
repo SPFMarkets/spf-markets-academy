c1b6fb1876fae6cddb5f4abcc71e9be0f4af215b
SPFMarkets <smalylsean@gmail.com>
2026-09-17 12:42:22 +0300

    Add path alias to vite configuration
    Added path alias resolution for easier imports.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev
 export default defineConfig({
-  // ADD THIS LINE BELOW TO FIX GITHUB PAGES PATHS
   base: "/spf-markets-academy/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
